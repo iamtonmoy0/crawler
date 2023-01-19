cb2ff85cf97508224625e8befb469cfeacfc1ba2
iamtonmoy0 <tonmoy2code@gmail.com>
2023-01-19 18:53:13 +0600

    goquery package installed

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	blogTitles, err := GetLatestBlogTitles("https://golangcode.com")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Blog Titles:")
+	fmt.Printf(blogTitles)
+
+}
+func GetLatestBlogTitles(url string) (string, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return "", err
+	}
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
+	}
 }
